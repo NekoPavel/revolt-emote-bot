9cb64426b5ff9b5b12f52aefbb07c5054fb46906
NekoPavel <minecraftpavel@gmail.com>
2023-07-17 15:32:01 +0200

    Fixed out of bounds issue

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using System.Reflection.Metadata;
 
 public class Program
 {
-    public static readonly bool debug = bool.Parse(Environment.GetEnvironmentVariable("BOT_DEBUG"));
+    public static readonly bool debug = bool.Parse(Environment.GetEnvironmentVariable("BOT_DEBUG")??"False");
     static void Main(string[] args)
     {
         Start().GetAwaiter().GetResult();
@@ -17,6 +17,7 @@ public class Program
     public static async Task Start()
     {
 
+        Environment.SetEnvironmentVariable("REVOLT_BOT_TOKEN", "ONqeoPMOF-TiKj0zG9CskOlrxy4-div8_QHKxth57r80SFdtfZNJTQqqOR5duvMP");
         string token = Environment.GetEnvironmentVariable("REVOLT_BOT_TOKEN") ?? throw new ArgumentException("No token in enviroment");
         Client = new RevoltClient(token, ClientMode.WebSocket, new ClientConfig
         {
@@ -107,7 +108,10 @@ public partial class AddEmoteCmd : ModuleBase
     public async Task Yoink([Remainder] string input)
     {
         string emoteLink = input.Split(" ")[0];
-        bool.TryParse(input.Split(" ")[1], out bool addAnyway);
+        bool addAnyway = false;
+        if (input.Split(" ").Length > 1) {
+            bool.TryParse(input.Split(" ")[1], out addAnyway);
+        }
         if (emoteLink == null)
         {
             await ReplyAsync("You need to provide an emote to yoink! ");
